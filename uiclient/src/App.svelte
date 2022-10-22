d12174121acfc1a18a2a7bcdd2ae43ef2f3e01a4
Dancho Atanasov <dancho.d.atanasov@gmail.com>
2022-10-22 18:12:46 +0100

    Added new ui client

---
+++ uiclient/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import PlayButton from './lib/PlayButton.svelte';
+</script>
+
+<main>
+  <h1>Welcome to Blackjack</h1>
+
+  <div class="card">
+    <PlayButton />
+  </div>
+
+</main>
+
+<style>
+</style>
---
+++ uiclient/src/lib/PlayButton.svelte
@@ -0,0 +1,25 @@
+
+<script>
+  import { v4 as uuidv4 } from 'uuid';
+  const play = () => {
+    console.log("Button clicked")
+    // Create WebSocket connection.
+    const socket = new WebSocket('ws://localhost:8080');
+
+    let randomId = uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+    // Connection opened
+    socket.addEventListener('open', (event) => {
+        socket.send(`{"Token": "${randomId}"}`);
+    });
+
+    // Listen for messages
+    socket.addEventListener('message', (event) => {
+        console.log('Message from server ', event.data);
+    });
+  }
+</script>
+
+<button on:click={play}>
+  Play
+</button>
+  
