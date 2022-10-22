b7f2d505639f16dda9bbce2943eb8db35dd37eb9
Dancho Atanasov <dancho.d.atanasov@gmail.com>
2022-10-22 10:35:21 +0100

    Refactored client

--- client/client.go
+++ client/client.go
@@ -16,15 +16,6 @@ import (
 	"github.com/gobwas/ws/wsutil"
 )
 
-type Player struct {
-	Name  string
-	BuyIn int
-}
-type BlackjackServerDetails struct {
-	GameServer string
-	Token      string
-}
-
 func getEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
@@ -32,8 +23,16 @@ func getEnv(key, fallback string) string {
 	return fallback
 }
 
-var hostName string = getEnv("BJ_HOST", "localhost")
 var API_SERVER_HOST string = getEnv("API_SERVER_HOST", "localhost")
+
+type Player struct {
+	Name  string
+	BuyIn int
+}
+type GameDetails struct {
+	GameServer string
+	Token      string
+}
 
 func sendData(conn net.Conn, msg string) (string, error) {
 	err := wsutil.WriteClientText(conn, []byte(msg))
@@ -41,66 +40,91 @@ func sendData(conn net.Conn, msg string) (string, error) {
 		fmt.Printf("Send failed")
 		return "", err
 	}
-	// fmt.Println("Sent ", msg)
 	return "OK", err
 }
 
-func readData(conn net.Conn, i int) (string, error) {
+func readData(conn net.Conn) (string, error) {
 	msg_bytes, err := wsutil.ReadServerText(conn)
 	if err != nil {
-		fmt.Printf("[%d] Receive failed, err %s\n", i, err)
+		fmt.Printf("Receive failed: %s\n", err)
 		return "", err
 	}
 	msg := string(msg_bytes)
-	// fmt.Println("Received ", msg)
 	return msg, err
 }
 
-func play(i int, wg *sync.WaitGroup, player Player, serverDetails BlackjackServerDetails) {
-	// TODO: break up function as it's too large
-	defer wg.Done()
-
-	// Start websocket connection to blackjack server
-	conn, _, _, err := ws.DefaultDialer.Dial(
-		context.Background(),
-		fmt.Sprintf("ws://%s/", serverDetails.GameServer),
-	)
-	if err != nil {
-		fmt.Printf("[%d] can not connect: %v\n", i, err)
-		return
-	}
-	//defer conn.Close() // TODO: maybe add
-
-	fmt.Printf("[%s] connected\n", player.Name)
-
+func sendToken(conn net.Conn, gameDetails GameDetails) {
 	type Token struct {
 		Token string
 	}
+
 	token := Token{
-		Token: serverDetails.Token,
+		Token: gameDetails.Token,
 	}
-	tokenBytes, err := json.Marshal(token)
+	tokenJson, _ := json.Marshal(token)
+
+	_, err := sendData(conn, string(tokenJson))
 	if err != nil {
-		fmt.Printf("[%d] can not marshal token: %v\n", i, err)
+		fmt.Printf("Sending token failed: %v\n", err)
 	}
+}
 
-	fmt.Printf("[%d] sending token to blackjack server\n", i)
-	_, err = sendData(conn, string(tokenBytes))
+func waitForStartMessage(conn net.Conn) {
+	startMsg, err := readData(conn)
 	if err != nil {
-		fmt.Printf("[%d] can not send details: %v\n", i, err)
+		fmt.Printf("Could not receive start message: %s\n", err)
 	}
-
-	fmt.Printf("[%d] Waiting for game to begin\n", i)
-
-	startMsg, err := readData(conn, i)
 	if startMsg != "Start" {
-		fmt.Printf("[%d] Wrong start msg received: %s\n", i, startMsg)
+		fmt.Printf("Wrong start msg received: %s\n", startMsg)
 	}
-	fmt.Printf("[%d] Game started\n", i)
+	// TODO: Add retry
+}
 
-	// Round loop
+func playHand(conn net.Conn, i int) {
 	for {
-		dealerHand, err := readData(conn, i)
+		currentCountString, err := readData(conn)
+		if err != nil {
+			break
+		}
+
+		if currentCountString == "Blackjack" {
+			fmt.Printf("[%d] got Blackjack!\n", i)
+			break
+		}
+
+		if currentCountString == "Bust" {
+			fmt.Printf("[%d] Bust\n", i)
+			break
+		}
+
+		currentCount, err := strconv.Atoi(currentCountString)
+		if err != nil {
+			fmt.Printf("[%d] Error converting count. %v\n", i, err)
+			break
+		}
+
+		fmt.Printf("[%d]Current hand: %d\n", i, currentCount)
+		var action string
+		if currentCount < 16 {
+			action = "H"
+		} else {
+			action = "S"
+		}
+
+		_, err = sendData(conn, action)
+		if err != nil {
+			break
+		}
+
+		if action == "S" {
+			break
+		}
+	}
+}
+
+func playRound(conn net.Conn, i int) {
+	for {
+		dealerHand, err := readData(conn)
 		if err != nil {
 			fmt.Println("Reading dealer hand failed, exiting")
 			break
@@ -111,82 +135,67 @@ func play(i int, wg *sync.WaitGroup, player Player, serverDetails BlackjackServe
 			fmt.Println("Game is over, ending")
 			break
 		}
-
-		for {
-			currentCountString, err := readData(conn, i)
-			if err != nil {
-				break
-			}
-
-			if currentCountString == "Blackjack" {
-				fmt.Printf("[%d] got Blackjack!\n", i)
-				break
-			}
-
-			if currentCountString == "Bust" {
-				fmt.Printf("[%d] Bust\n", i)
-				break
-			}
-
-			currentCount, err := strconv.Atoi(currentCountString)
-			if err != nil {
-				fmt.Printf("[%d] Error converting count. %v\n", i, err)
-				break
-			}
-
-			fmt.Printf("[%d]Current hand: %d\n", i, currentCount)
-			var action string
-			if currentCount < 16 {
-				action = "H"
-			} else {
-				action = "S"
-			}
-
-			_, err = sendData(conn, action)
-			if err != nil {
-				break
-			}
-
-			if action == "S" {
-				break
-			}
-		}
-	}
-
-	err = conn.Close()
-	if err != nil {
-		fmt.Printf("[%d] can not close: %v\n", i, err)
-	} else {
-		fmt.Printf("[%d] closed\n", i)
+		playHand(conn, i)
 	}
 }
 
-func findServer(player Player) BlackjackServerDetails {
-	postBody, _ := json.Marshal(player)
-	responseBody := bytes.NewBuffer(postBody)
-	// TODO: change hostname to env variable
+func play(i int, wg *sync.WaitGroup, player Player, gameDetails GameDetails) {
+	defer wg.Done()
+
+	// Start websocket connection to blackjack server
+	conn, _, _, err := ws.DefaultDialer.Dial(
+		context.Background(),
+		fmt.Sprintf("ws://%s/", gameDetails.GameServer),
+	)
+	if err != nil {
+		fmt.Printf("[%d] can not connect: %v\n", i, err)
+		return
+	}
+	defer conn.Close() // TODO: maybe add
+
+	fmt.Printf("[%s] connected\n", player.Name)
+
+	fmt.Printf("[%d] sending token to blackjack server\n", i)
+	sendToken(conn, gameDetails)
+
+	fmt.Printf("[%d] Waiting for game to begin\n", i)
+	waitForStartMessage(conn)
+
+	fmt.Printf("[%d] Game started\n", i)
+
+	playRound(conn, i)
+}
+
+// registerPlayer send the player details to the API server.
+// It returns the game server host name and session token.
+func registerPlayer(player Player) GameDetails {
+	playerJson, _ := json.Marshal(player)
+	requestBody := bytes.NewBuffer(playerJson)
 	resp, err := http.Post(
 		fmt.Sprintf("http://%s:3333/play", API_SERVER_HOST),
 		"application/json",
-		responseBody,
+		requestBody,
 	)
 	if err != nil {
-		fmt.Printf("An Error Occured %v\n", err)
+		fmt.Printf("Could not register player: %v\n", err)
 		os.Exit(1)
 	}
 	defer resp.Body.Close()
 
-	var gameDetails BlackjackServerDetails
+	var gameDetails GameDetails
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println(err)
-	}
-	err = json.Unmarshal([]byte(body), &gameDetails)
-	if err != nil {
-		fmt.Println("could not unmarshal body")
+		fmt.Printf("Could not read response body %v\n", err)
+		os.Exit(1)
 	}
 
-	fmt.Printf("got %s, %s\n", gameDetails.GameServer, gameDetails.Token)
+	err = json.Unmarshal([]byte(body), &gameDetails)
+	if err != nil {
+		fmt.Printf("Response body in wrong format: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Registerred successfully, game details %v\n", gameDetails)
 	return gameDetails
 }
 
@@ -195,12 +204,14 @@ func main() {
 	numPlayers := 6
 	for i := 0; i < numPlayers; i++ {
 		wg.Add(1)
+
 		player := Player{
 			Name:  fmt.Sprintf("Player %d", i),
 			BuyIn: 100 * (i + 1),
 		}
-		blackjackServerDetails := findServer(player)
-		go play(i, &wg, player, blackjackServerDetails)
+		gameDetails := registerPlayer(player)
+
+		go play(i, &wg, player, gameDetails)
 	}
 	wg.Wait()
 }
