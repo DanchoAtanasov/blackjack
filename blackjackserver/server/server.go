9e76b35aac3df9a083f7d2d7c0c1e5dd11b01bcd
Dancho Atanasov <dancho.d.atanasov@gmail.com>
2022-11-19 17:47:34 +0000

    Refactored room

--- blackjackserver/server/server.go
+++ blackjackserver/server/server.go
@@ -6,13 +6,10 @@ import (
 	"io"
 	"net"
 	"net/http"
-	"os"
 	"sync"
 
 	"github.com/gobwas/ws"
 	"github.com/gobwas/ws/wsutil"
-	"github.com/google/uuid"
-	"github.com/sirupsen/logrus"
 
 	settings "blackjack/config"
 	"blackjack/models"
@@ -38,73 +35,6 @@ func SendData(conn net.Conn, msg string) {
 	if err != nil {
 		fmt.Println("Send failed, ", err)
 	}
-}
-
-// Struct used to couple the player model and the connection to him
-type PlayerConn struct {
-	player models.Player
-	Conn   net.Conn
-}
-
-type Room struct {
-	Log         *logrus.Logger
-	Id          string
-	playerConns []PlayerConn
-}
-
-func MakeRoom() *Room {
-	room := Room{}
-	room.Id = uuid.New().String()
-	room.Log = MakeLog(room.Id)
-	room.playerConns = make([]PlayerConn, 0, settings.RoomSize)
-
-	return &room
-}
-
-func MakeLog(id string) *logrus.Logger {
-	filename := fmt.Sprintf("./logs/%s.log", id)
-	log := logrus.New()
-	log.Out = os.Stdout
-
-	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err == nil {
-		log.Out = file
-	} else {
-		log.Info("Failed to log to file, using default stderr")
-	}
-
-	return log
-}
-
-func (room *Room) GetCurrPlayerConn() *PlayerConn {
-	return &room.playerConns[0]
-}
-
-func (room *Room) RemoveDisconnectedPlayer() {
-	// TODO improve player diconnecting/rotating logic
-	// maybe couple the player and room objects better
-	room.playerConns = room.playerConns[1:]
-}
-
-func (room *Room) ChangePlayer() {
-	// Change players by popping from queue and appending
-	var currentConn PlayerConn
-	currentConn, room.playerConns = room.playerConns[0], room.playerConns[1:]
-	room.playerConns = append(room.playerConns, currentConn)
-}
-
-func (room *Room) SendAll(msg string) {
-	for _, playerConnection := range room.playerConns {
-		SendData(playerConnection.Conn, msg)
-	}
-}
-
-func (room *Room) GetPlayers() []models.Player {
-	var players []models.Player
-	for i := 0; i < len(room.playerConns); i++ {
-		players = append(players, room.playerConns[i].player)
-	}
-	return players
 }
 
 type Server struct {
