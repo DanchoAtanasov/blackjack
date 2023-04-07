99fa53fede66fdac2b6e8edcfc7e7d4958d70598
DanchoAtanasov <dancho.d.atanasov@gmail.com>
2023-04-07 20:55:26 +0100

    Add create users table on connect

--- apiserver/database.go
+++ apiserver/database.go
@@ -22,8 +22,8 @@ const (
 	maxWaitInterval = 3 * time.Second
 )
 
+// TODO improve database abstraction
 func connectToDatabase() error {
-	// connection string
 	psqlconn := fmt.Sprintf(
 		"host=%s port=%d user=%s password=%s sslmode=disable",
 		host, port, user, password,
@@ -61,6 +61,41 @@ func connectToDatabase() error {
 
 	// close database
 	defer db.Close()
+	fmt.Println("Connected")
+
+	fmt.Println("Checking if users table exists")
+	_, err = db.Exec(`SELECT 1 FROM users;`)
+	if err == nil {
+		fmt.Println("Users table exists, conneciton complete")
+		return nil
+	}
+
+	fmt.Println("Users table does not exist, creating...")
+	_, err = db.Exec(`
+		CREATE TABLE users(
+			id INT PRIMARY KEY,
+			username VARCHAR(50) UNIQUE NOT NULL,
+			password VARCHAR(100) NOT NULL
+		);`,
+	)
+	if err != nil {
+		fmt.Printf("error creating table %s\n", err)
+		return errors.New("Cant create table")
+	}
+	fmt.Println("Users table created")
+
+	fmt.Println("Create root user")
+	_, err = db.Exec(`
+		INSERT INTO users(
+			id, username, password
+		) VALUES (
+			1, 'root', 'pass'
+		);`,
+	)
+	if err != nil {
+		fmt.Printf("error creating root user %s\n", err)
+		return errors.New("Cant create root user")
+	}
 
 	fmt.Println("Connected to database!")
 	return nil
